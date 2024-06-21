103b44f1dc11d3846e08f35b471c7c27eaba9415
pshwuli <121034439+pshwuli@users.noreply.github.com>
2024-06-21 13:55:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { logger } from '#Karin'
-import { basename, Config } from '#template'
+import { logger } from '#bili'
 
 logger.info(`${logger.violet(`[插件:${Config.package.version}]`)} ${logger.green(basename)} 初始化完成~`)
